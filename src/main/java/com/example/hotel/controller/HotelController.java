1b45a2d5ada8944d081e914568bdeef969856e59
Krishan Pareek <krishanpareek809@gmail.com>
2024-01-22 22:38:51 +0530

    first commit

---
+++ src/main/java/com/example/hotel/controller/HotelController.java
@@ -0,0 +1,37 @@
+package com.example.hotel.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.hotel.model.Hotel;
+import com.example.hotel.service.HotelService;
+
+@RestController
+@RequestMapping("/hotel")
+public class HotelController {
+
+    @Autowired
+    HotelService hotelService;
+
+    @PostMapping("/create")
+    public void createHotel(@RequestBody Hotel hotel){
+        hotelService.createHotel(hotel);
+    }
+
+    @GetMapping("/id/{id}")
+    public Hotel getHotelById(@PathVariable String id){
+        return hotelService.getHotelById(id);
+    }
+
+    @GetMapping("/getAll")
+    public List<Hotel> getAllHotels(){
+        return hotelService.getAllHotels();
+    }
+}
